4f7c410381e216648285c11545dd5ae03ae244b8
Michael Williams <mwilliams11175@gmail.com>
2015-06-29 21:58:29 -0500

    added profile page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,6 +6,6 @@ class RegistrationsController < Devise:: RegistrationsController
 	end
 
 	def account_update_params
-		params.require(:user).permit(:email, :password, :password_confirmation, :username, :current_password)
+		params.require(:user).permit(:email, :age, :gender, :bio, :password, :password_confirmation, :username, :current_password)
 	end
 end
