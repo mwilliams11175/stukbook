50678be5ff9a863c5af1c72d6d9cb82348a4b8d3
Michael Williams <mwilliams11175@gmail.com>
2015-07-09 21:04:34 -0500

    added database seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(username: "michaelwilliams", email: "michael.williams@gmail.com", password: "password", password_confirmation: "password")
+User.create(username: "michaelwilliams1", email: "michael.williams1@gmail.com", password: "password", password_confirmation: "password")
+User.create(username: "michaelwilliams2", email: "michael.williams2@gmail.com", password: "password", password_confirmation: "password")
+User.create(username: "michaelwilliams3", email: "michael.williams3@gmail.com", password: "password", password_confirmation: "password")
+User.create(username: "michaelwilliams4", email: "michael.williams4@gmail.com", password: "password", password_confirmation: "password")
+User.create(username: "michaelwilliams5", email: "michael.williams5@gmail.com", password: "password", password_confirmation: "password")
+User.create(username: "michaelwilliams6", email: "michael.williams6@gmail.com", password: "password", password_confirmation: "password")
+User.create(username: "michaelwilliams7", email: "michael.williams7@gmail.com", password: "password", password_confirmation: "password")
